79bd79a0d96b4979cf44fd661d25f3baf77bef67
garun <you@example.com>
2026-09-19 15:15:22 +0400

    fix: load Manrope font bundle correctly

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '@fontsource-variable/manrope/cyrillic.css'
+import '@fontsource-variable/manrope'
 import './styles/tokens.css'
 import './styles/global.css'
 import './styles/components.css'
